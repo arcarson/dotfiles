e652ecfcd408f76c30ca7e40eb1c37afefee4bb9
Andy Carson <andrew.carson@silverpond.com.au>
2014-03-24 09:40:32 +1100

    added slate config

--- dotfile-installer.sh
+++ dotfile-installer.sh
@@ -8,5 +8,6 @@ cp -v dot.aliases ~/.aliases
 cp -v dot.bash_profile ~/.bash_profile
 cp -v dot.gitconfig ~/.gitconfig
 cp -v dot.gitignore ~/.gitignore
+cp -v dot.slate ~/.slate
 cp -v dot.vimrc ~/.vimrc
 
