8fbb2a5b7a0b0077058423c1acb5ff78b601127d
Andy Carson <andrew.carson@silverpond.com.au>
2015-01-10 11:44:16 +1100

    updated cask formula

--- brew-installer.sh
+++ brew-installer.sh
@@ -7,26 +7,28 @@ echo "upgrading current formulae..."
 brew upgrade
 
 echo "installing brew formulae..."
-#brew install apple-gcc42
 brew install bash-completion
 brew install fontforge ttfautohint
 brew install git
 brew install heroku-toolbelt
 brew install imagemagick
-#brew install mysql
 brew install tig
 brew install vim
 
-#echo "setting up brew-cask..."
-#brew tap phinze/cask
-#brew install brew-cask
+echo "setting up brew-cask..."
+brew install caskroom/cask/brew-cask
 
 echo "installing casks..."
 brew cask install alfred
 brew cask install google-chrome
 brew cask install iterm2
-brew cask install keyremap4macbook
 brew cask install macvim
-brew cask install pckeyboardhack
-brew cask install skype
 brew cask install slate
+brew cask install seil
+brew cask install karabiner
+brew cask install vlc
+brew cask install virtualbox
+brew cask install slack
+brew cask install firefox
+brew cask install adobe-creative-cloud
+
