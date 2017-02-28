05af24976e0aad30e00958e21a1e8753c8edb6c2
Andy Carson <andrew.carson@silverpond.com.au>
2017-03-01 07:22:48 +1100

    1/3 + 2/3 tile keys

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -6,6 +6,34 @@ local mash = {"cmd", "alt", "ctrl", "shift"}
 
 -- Window positioning
 
+hs.hotkey.bind(mash, "1", function()
+  local win    = hs.window.focusedWindow()
+  local f      = win:frame()
+  local screen = win:screen()
+  local max    = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w * .33333
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+
+hs.hotkey.bind(mash, "2", function()
+  local win    = hs.window.focusedWindow()
+  local f      = win:frame()
+  local screen = win:screen()
+  local max    = screen:frame()
+
+  f.x = max.x + (max.w * .33333)
+  f.y = max.y
+  f.w = max.w * .66666
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+
 hs.hotkey.bind(mash, "h", function()
   local win    = hs.window.focusedWindow()
   local f      = win:frame()
