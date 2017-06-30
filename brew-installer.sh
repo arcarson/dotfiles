384cfa054d1ed0d7c454dc6d022d6bc6fdaf680f
Andy Carson <andrew.carson@silverpond.com.au>
2017-06-30 16:21:35 +1000

    updated install script

--- brew-installer.sh
+++ brew-installer.sh
@@ -8,28 +8,26 @@ brew upgrade
 
 echo "installing brew formulae..."
 brew install bash-completion
-brew install fontforge ttfautohint
 brew install git
 brew install heroku-toolbelt
 brew install imagemagick
+brew install node
 brew install postgresql
+brew install ruby-build
 brew install tig
 brew install vim
-brew install node
-brew install ruby-build
 
 echo "setting up brew-cask..."
 brew install caskroom/cask/brew-cask
 
 echo "installing casks..."
-brew cask install google-chrome
-brew cask install iterm2
-brew cask install macvim
-brew cask install hammerspoon
-brew cask install seil
-brew cask install karabiner
-brew cask install vlc
-brew cask install virtualbox
-brew cask install slack
-brew cask install firefox
 brew cask install adobe-creative-cloud
+brew cask install firefox
+brew cask install google-chrome
+brew cask install hammerspoon
+brew cask install iterm2
+brew cask install karabiner-elements
+brew cask install macvim
+brew cask install sketch
+brew cask install slack
+brew cask install virtualbox
